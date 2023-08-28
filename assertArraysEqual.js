7adc4084c216b8ac5846e0f9ff412739488f41c9
Andrew On <a_on93@hotmail.com>
2023-08-27 21:38:41 -0400

    created function displaying statement for comparing two arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,23 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected === false)){
+    statement = console.log(`Assertion Failed:  ${actual} !==  ${expected}`);
+  }
+    else{ 
+      statement = console.log(`Assertion Passed:  ${actual} ===  ${expected}`);
+    }
+    return statement;
+};
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length){
-    statement = console.log(`Assertion Failed:  ${actual} !==  ${expected}`);
+    return false;
   }
     for (let x = 0; x < actual.length; x++){
       if (actual[x] !== expected[x]){
-        statement = console.log(`Assertion Failed:  ${actual} !==  ${expected}`);
+        return false;
       }
     }
-    statement = console.log(`Assertion Passed:  ${actual} ===  ${expected}`);
-    return statement;
+  return true;
 };
-
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
