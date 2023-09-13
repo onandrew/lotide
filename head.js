e1fcf431b29b3b48569740fddeac5b8389927c4f
Andrew On <a_on93@hotmail.com>
2023-09-13 15:07:19 -0400

    refactored head and tail functions

--- head.js
+++ head.js
@@ -16,7 +16,4 @@ const head = function(x){
   return first;
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+module.exports = head;
--- tail.js
+++ tail.js
@@ -1,13 +1,6 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  let statement = '';
-  if (actual === expected) {
-    statement = console.log(`Assertion Passed:  ${actual} ===  ${expected}`);
-  } else {
-    statement = console.log(`Assertion Failed:  ${actual} !==  ${expected}`);
-  }
-  return statement;
-};
+const assertEqual = require('./assertEqual');
+
 
 const tail = function(x) {
   let newArray = [];
@@ -21,10 +14,4 @@ const tail = function(x) {
 };
 
 // TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
