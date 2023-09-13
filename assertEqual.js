b610b63ddf969802986d8194353a387e06e03599
Andrew On <a_on93@hotmail.com>
2023-09-13 13:48:27 -0400

    refactored head and assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 const assertEqual = function(actual, expected) {
   let statement = '';
   if (actual === expected) {
-    statement = console.log("Assertion Passed: " + actual + " === " + expected);
+    statement = console.log(`Assertion Passed:  ${actual} ===  ${expected}`);
   } else {
-    statement = console.log("Assertion Failed: " + actual + " !== " + expected);
+    statement = console.log(`Assertion Failed:  ${actual} !==  ${expected}`);
   }
   return statement;
 };
