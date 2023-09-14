953d5f451b3fa40215f77451dba88499b5a01f96
Andrew On <a_on93@hotmail.com>
2023-09-14 09:48:15 -0400

    added in assertion testing with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,4 @@
-const assertEqual = require('../assertEqual');
+/* const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
@@ -8,3 +8,16 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+*/
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [] for [5]", () => {
+    assert.deepEqual(tail([5]), []); 
+  });
+});
