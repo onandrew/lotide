77d841c5bc3b2ea747d815ca2e676760c79114f0
Andrew On <a_on93@hotmail.com>
2023-09-14 11:12:20 -0400

    added extra functions to index require

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 module.exports = {
-  head, tail, middle,
+  head, tail, middle, assertArraysEqual, assertEqual, eqArrays,
 };
