00e6b0cf9d4f27b34e172b11b32fee0c529517ef
Andrew On <a_on93@hotmail.com>
2023-09-18 14:59:39 -0400

    updated without function to accurately filter out larger arrays

--- without.js
+++ without.js
@@ -1,13 +1,11 @@
 const without = function(source, itemsToRemove) {
-  let outputArray = source;
-  for (let i = 0; i < source.length; i++) {
-    for (let j = 0; j < itemsToRemove.length; j++) {
-      if (source[i] === itemsToRemove[j]) {
-        outputArray.splice(i, 1);
-      }
+  let newArray = [];
+  for (let x = 0; x < source.length; x++) {
+    if (!itemsToRemove.includes(source[x])) {
+      newArray.push(source[x]);
     }
   }
-  return source;
+  return newArray;
 };
 
 const eqArrays = function(actual, expected) {
@@ -32,6 +30,9 @@ const assertArraysEqual = function(actual, expected) {
 };
 assertArraysEqual(without([1,2,3], [1]), [2,3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+
+assertArraysEqual(without(["1", "1", "1", "2", "3", "1" , "1"], ["1", 2, "3"]) , ["2"]); 
+
 
 const words = ["hello", "world", "lighthouse"];
 without(["hello", "world", "lighthouse"], ["lighthouse"]); 
